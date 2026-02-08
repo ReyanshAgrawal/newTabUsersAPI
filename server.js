9187fe1e1ca27034bac5a7806d2ea414e1818803
ReyanshAgrawal <rag2142@gmail.com>
2026-02-08 15:08:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const mongoose = require("mongoose")
 const User = require("./models/user.model.js")
 const app = express()
